01ad31eef28045eadb591ab706b0f2ac7d5e9ca1
Kamnev Georgiy <nt.gocha@gmail.com>
2022-02-23 02:21:13 +0500

    flat map

--- jtfm/src/test/scala/Test1.scala
+++ jtfm/src/test/scala/Test1.scala
@@ -2,5 +2,11 @@ import org.junit.Test
 import org.junit.Assert.*
 
 class Test1:
-  @Test def t1(): Unit = 
-    assertEquals("I was compiled by Scala 3. :)", msg)
+  @Test def t1(): Unit =
+    val l = List( "1", "2", "aa", "3" )
+    val x = l.flatMap(_.toIntOption)
+    val y = for {
+      i <- x;
+      j <- x
+    } yield (i,j)
+    y foreach { (a,b) => println(s"${a} ${b}") }
