02a7e7cef79f64b3616b21ffc02a19c696da6ed8
Kamnev Georgiy <nt.gocha@gmail.com>
2022-02-23 02:22:08 +0500

    parse command line

--- jtfm/src/main/scala/xyz/cofe/jtfm/Main.scala
+++ jtfm/src/main/scala/xyz/cofe/jtfm/Main.scala
@@ -1,9 +1,54 @@
 package xyz.cofe.jtfm
 
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory
+
+enum ParseState:
+  case Init
+  case TelnetPort
+  case TelnetUse
+
+case class CmdLineOpt(
+                       state: ParseState = ParseState.Init,
+                       telnetPort: Int = 4044,
+                       telnetStart: Boolean = false,
+                       unparsed: List[String] = List()
+                     )
+
+object CmdLineOpt:
+  
+  import ParseState._
+  
+  def parse(args: Seq[String]): CmdLineOpt =
+    args.foldLeft(CmdLineOpt())((opt, arg) => {
+      opt.state match {
+        case Init => arg match {
+          case "-telnet.port" | "-t.port" => opt.copy(state = TelnetPort, telnetStart = true)
+          case "-telnet" => opt.copy(state = TelnetUse)
+          case _ => opt.copy(unparsed = opt.unparsed ::: List(arg))
+        }
+        case TelnetPort => opt.copy(telnetPort = arg.toInt, state = Init)
+        case TelnetUse => arg match {
+          case "yes" | "true" | "on" => opt.copy(state = Init, telnetStart = true)
+          case "no" | "false" | "off" => opt.copy(state = Init, telnetStart = false)
+          case s if s.matches("\\d+") => opt.copy(state = Init, telnetStart = true, telnetPort = s.toInt)
+          case _ => opt.copy(unparsed = opt.unparsed ::: List(arg), state = Init)
+        }
+      }
+    })
+
 @main def hello(args: String*): Unit =
-  println("Hello world!")
-  println(s"args(${args.length})")
-  args.zip(0 until args.length).foreach{ (arg,argi) => {
-    println(s"arg[$argi] = $arg")
-  }}
+  CmdLineOpt.parse(args) match {
+    case CmdLineOpt(_, telnetPort, true, _) =>
+      println(s"start telnet here on port $telnetPort")
+    case CmdLineOpt(_, _, false, _) =>
+      println("start as is")
+    case _ =>
+      throw new Error("undefined beheavior")
+  }
+
+//startDefault()
+
+def startDefault(): Unit =
+  Session(DefaultTerminalFactory().createTerminal())
+
 
