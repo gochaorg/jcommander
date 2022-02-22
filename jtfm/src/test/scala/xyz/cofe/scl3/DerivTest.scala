4ea23e4e9437e7478f49cb8e6a41a1126bd3f2b1
Kamnev Georgiy <nt.gocha@gmail.com>
2022-02-23 02:34:50 +0500

    easy derive

--- jtfm/src/test/scala/xyz/cofe/scl3/DerivTest.scala
+++ jtfm/src/test/scala/xyz/cofe/scl3/DerivTest.scala
@@ -1,145 +1,137 @@
 package xyz.cofe.scl3
 
+// https://blog.philipp-martini.de/blog/magic-mirror-scala3/
 import org.junit.Test
 
-// https://blog.philipp-martini.de/blog/magic-mirror-scala3/
-
 import scala.deriving.Mirror
+import scala.compiletime.{constValue, erasedValue, summonInline}
+
+/**************************** trait to be used for simple derivation below **************************/
+
+// this traits can just be copy/pasted or reside in a library
+trait EasyDerive[TC[_]] {
+  final def apply[A](using tc: TC[A]): TC[A] = tc
+  
+  case class CaseClassElement[A, B](label: String, typeclass: TC[B], getValue: A => B, idx: Int)
+  case class CaseClassType[A](label: String, elements: List[CaseClassElement[A, _]], fromElements: List[Any] => A)
+  
+  case class SealedElement[A, B](label: String, typeclass: TC[B], idx: Int, cast: A => B)
+  case class SealedType[A](label: String, elements: List[SealedElement[A, _]], getElement: A => SealedElement[A, _])
+  
+  inline def getInstances[A <: Tuple]: List[TC[Any]] = inline erasedValue[A] match {
+    case _: EmptyTuple => Nil
+    case _: (t *: ts) => summonInline[TC[t]].asInstanceOf[TC[Any]] :: getInstances[ts]
+  }
+  
+  inline def getElemLabels[A <: Tuple]: List[String] = inline erasedValue[A] match {
+    case _: EmptyTuple => Nil
+    case _: (t *: ts) => constValue[t].toString :: getElemLabels[ts]
+  }
+  
+  def deriveCaseClass[A](caseClassType: CaseClassType[A]): TC[A]
+  
+  def deriveSealed[A](sealedType: SealedType[A]): TC[A]
+  
+  inline given derived[A](using m: Mirror.Of[A]) : TC[A] = {
+    val label = constValue[m.MirroredLabel]
+    val elemInstances = getInstances[m.MirroredElemTypes]
+    val elemLabels = getElemLabels[m.MirroredElemLabels]
+    
+    inline m match {
+      case s: Mirror.SumOf[A] =>
+        val elements = elemInstances.zip(elemLabels).zipWithIndex.map{ case ((inst, lbl), idx) =>
+          SealedElement[A, Any](lbl, inst.asInstanceOf[TC[Any]], idx, identity)
+        }
+        val getElement = (a: A) => elements(s.ordinal(a))
+        deriveSealed(SealedType[A](label, elements, getElement))
+      
+      case p: Mirror.ProductOf[A] =>
+        val caseClassElements =
+          elemInstances
+            .zip(elemLabels)
+            .zipWithIndex.map{ case ((inst, lbl), idx) =>
+            CaseClassElement[A, Any](lbl, inst.asInstanceOf[TC[Any]],
+              (x: Any) => x.asInstanceOf[Product].productElement(idx), idx)
+          }
+        val fromElements: List[Any] => A = { elements =>
+          val product: Product = new Product {
+            override def productArity: Int = caseClassElements.size
+            
+            override def productElement(n: Int): Any = elements(n)
+            
+            override def canEqual(that: Any): Boolean = false
+          }
+          p.fromProduct(product)
+        }
+        deriveCaseClass(CaseClassType[A](label, caseClassElements, fromElements))
+    }
+  }
+}
+
+/**************************** define typeclass and derivation **************************/
 
 trait PrettyString[A] {
-  def prettyString(a: A): String // will print a type A in a pretty, human readable way
+  def prettyString(a: A): String
 }
 
-//val intPrettyString =
-//  new PrettyString[Int] {
-//    def prettyString(a: Int): String  = a.toString
-//  }
-//
-//val stringPrettyString = new PrettyString[String] {
-//  def prettyString(a: String): String = "\"" + a + "\""
-//}
-//
-//val booleanPrettyString = new PrettyString[Boolean] {
-//  def prettyString(a: Boolean): String = a.toString
-//}
-
-given intPrettyString: PrettyString[Int] with
-  def prettyString(a: Int): String = a.toString
-
-given longPrettyString: PrettyString[Long] with
-  def prettyString(a: Long): String = a.toString
-
-given stringPrettyString: PrettyString[String] with
-  def prettyString(a: String): String = "\"" + a + "\""
-
-given booleanPrettyString: PrettyString[Boolean] with
-  def prettyString(a: Boolean): String = a.toString
-
-enum SiteMember:
-  case RegisteredUser(id: Long, email: String, isAdmin: Boolean)
-  case AnonymousUser(session: String)
+object PrettyString extends EasyDerive[PrettyString] {
+  override def deriveCaseClass[A](productType: CaseClassType[A]): PrettyString[A] = new PrettyString[A] {
+    override def prettyString(a: A): String = {
+      if (productType.elements.isEmpty) productType.label
+      else {
+        val prettyElements = productType.elements.map(p => s"${p.label}=${p.typeclass.prettyString(p.getValue(a))}")
+        prettyElements.mkString(s"${productType.label}(", ", ", ")")
+      }
+    }
+  }
+  override def deriveSealed[A](sumType: SealedType[A]): PrettyString[A] = new PrettyString[A] {
+    override def prettyString(a: A): String = {
+      val elem = sumType.getElement(a)
+      elem.typeclass.prettyString(elem.cast(a))
+    }
+  }
   
-import scala.compiletime.constValue
-inline def labelFromMirror[A](using m: Mirror.Of[A]): String = constValue[m.MirroredLabel]
-
-import scala.compiletime.erasedValue
-inline def getElemLabels[A <: Tuple]: List[String] = inline erasedValue[A] match {
-  case _: EmptyTuple => Nil // stop condition - the tuple is empty
-  case _: (head *: tail) =>  // yes, in scala 3 we can match on tuples head and tail to deconstruct them step by step
-    val headElementLabel = constValue[head].toString // bring the head label to value space
-    val tailElementLabels = getElemLabels[tail] // recursive call to get the labels from the tail
-    headElementLabel :: tailElementLabels // concat head + tail
+  // some instances for primitive types
+  
+  given stringPrettyString : PrettyString[String] with
+    def prettyString(x: String): String = s""""x""""
+  
+  given intPrettyString : PrettyString[Int] with
+    def prettyString(x: Int): String = x.toString
+  
+  given longPrettyString : PrettyString[Long] with
+    def prettyString(x: Long): String = x.toString
+  
+  given doublePrettyString : PrettyString[Double] with
+    def prettyString(x: Double): String = x.toString
+  
+  given booleanPrettyString : PrettyString[Boolean] with
+    def prettyString(x: Boolean): String = x.toString
+  
+  // our helper method to print the PrettyString
+  def prettyPrintln[A](a: A)(using prettyStringInstance: PrettyString[A]) =
+    println(prettyStringInstance.prettyString(a))
 }
 
-// helper method to get the mirror from compiler
-inline def getElemLabelsHelper[A](using m: Mirror.Of[A]) =
-  getElemLabels[m.MirroredElemLabels] // and call getElemLabels with the elemlabels type
+/************************************** Use it *****************************************/
 
-import scala.compiletime.summonInline
-inline def getTypeclassInstances[A <: Tuple]: List[PrettyString[Any]] = inline erasedValue[A] match {
-  case _: EmptyTuple => Nil
-  case _: (head *: tail) =>
-    val headTypeClass = summonInline[PrettyString[head]] // summon was known as implicitly in scala 2
-    val tailTypeClasses = getTypeclassInstances[tail] // recursive call to resolve also the tail
-    headTypeClass.asInstanceOf[PrettyString[Any]] :: getTypeclassInstances[tail]
-}
-
-// helper method like before
-inline def summonInstancesHelper[A](using m: Mirror.Of[A]) =
-  getTypeclassInstances[m.MirroredElemTypes]
-
-inline def derivePrettyStringCaseClass[A](using m: Mirror.ProductOf[A]) =
-  new PrettyString[A] {
-    def prettyString(a: A): String = {
-      val label = labelFromMirror[m.MirroredType]
-      val elemLabels = getElemLabels[m.MirroredElemLabels]
-      val elemInstances = getTypeclassInstances[m.MirroredElemTypes]
-      val elems = a.asInstanceOf[Product].productIterator // every case class implements scala.Product, we can safely cast here
-      val elemStrings = elems.zip(elemLabels).zip(elemInstances).map{
-        case ((elem, label), instance) => s"$label=${instance.prettyString(elem)}"
-      }
-      //s"$label(${elemStrings.mkString(", ")})"
-      if (elemLabels.isEmpty) { // check if this is a case object (or parameterless case class)
-        label
-      } else {
-        s"$label(${elemStrings.mkString(", ")})"
-      }
-    }
-  }
-
-inline def derivePrettyStringSealedTrait[A](using m: Mirror.SumOf[A]) =
-  new PrettyString[A] {
-    def prettyString(a: A): String = {
-      // val label = labelFromMirror[m.MirroredType] - not needed
-      // val elemLabels = getElemLabels[m.MirroredElemLabels] - not needed
-      val elemInstances = getTypeclassInstances[m.MirroredElemTypes] // same as for the case class
-      val elemOrdinal = m.ordinal(a) // Checks the ordinal of the type, e.g. 0 for User or 1 for AnonymousVisitor
-      
-      // just return the result of prettyString from the right element instance
-      elemInstances(elemOrdinal).prettyString(a)
-    }
-  }
-
-
-inline given derived[A](using m: Mirror.Of[A]): PrettyString[A] =
-  inline m match {
-    case s: Mirror.SumOf[A]     => derivePrettyStringSealedTrait(using s)
-    case p: Mirror.ProductOf[A] => derivePrettyStringCaseClass(using p)
-  }
-
-enum Visitor {
+enum Visitor derives PrettyString { // magic happens here via 'derives'
   case User(name: String, age: Int)
   case AnonymousVisitor
 }
 
+
 class DerivTest {
-  import Visitor._
-  
   @Test
   def test1: Unit = {
-    println("hello")
-    
-    val regUsr = SiteMember.RegisteredUser(10,"email",false)
-    println(regUsr)
-    println(labelFromMirror[SiteMember])
-    println(labelFromMirror[SiteMember.RegisteredUser])
-    println(getElemLabelsHelper[SiteMember.RegisteredUser])
-    
-    val fields = summonInstancesHelper[SiteMember.RegisteredUser]
-    println(fields)
+    import Visitor._
+    import PrettyString.prettyPrintln
   
-    val userPrettyString = derivePrettyStringCaseClass[User]
-    println(userPrettyString.prettyString(User("Bob",12)))
+    val someVisitors = List(
+      User("bob", 25),
+      AnonymousVisitor
+    )
   
-    val visitorPrettyString = derived[Visitor]
-    println(
-      visitorPrettyString.prettyString(User("Boba",14))
-    )
-    println(
-      visitorPrettyString.prettyString(AnonymousVisitor)
-    )
-    
-//    val sm = derivePrettyStringCaseClass[SiteMember]
-//    println(sm.prettyString(regUsr))
+    someVisitors.foreach(prettyPrintln)
   }
 }
