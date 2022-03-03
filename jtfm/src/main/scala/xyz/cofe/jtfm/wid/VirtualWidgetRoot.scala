07bb3ea9ee5a9867fa48439047ab783493decf49
Kamnev Georgiy <nt.gocha@gmail.com>
2022-03-04 03:05:03 +0500

    VirtualRoot

---
+++ jtfm/src/main/scala/xyz/cofe/jtfm/wid/VirtualWidgetRoot.scala
@@ -0,0 +1,6 @@
+package xyz.cofe.jtfm.wid
+
+/**
+ * Корень для виджетов
+ */
+final case class VirtualWidgetRoot() extends Widget[VirtualWidgetRoot]
--- jtfm/src/main/scala/xyz/cofe/jtfm/wid/WidgetCycle.scala
+++ jtfm/src/main/scala/xyz/cofe/jtfm/wid/WidgetCycle.scala
@@ -1,5 +1,40 @@
 package xyz.cofe.jtfm.wid
 
-class WidgetCycle {
+import com.googlecode.lanterna.terminal.Terminal
+
+/**
+ * Цикл управления событиями ввода и рендера.
+ * 
+ * <p>
+ * 1 поток = 1 цикл обработки
+ */
+class WidgetCycle( 
+    private val terminal: Terminal 
+) {
   
+}
+
+object WidgetCycle {
+    private val currentCycle : InheritableThreadLocal[WidgetCycle] =
+        new InheritableThreadLocal()
+
+    def apply( term: Terminal ):Either[WidgetCycle,Error] = {
+        val wc0 = currentCycle.get
+        if( wc0==null ){
+            val wc1 = new WidgetCycle(term)
+            currentCycle.set(wc1)
+            Left(wc1)
+        }else{
+            if( wc0.terminal==term ){
+                Left(wc0)
+            }else{
+                Right(new Error("has already created WidgetCycle for different Terminal"))
+            }
+        }
+    }
+
+    def tryGet:Option[WidgetCycle] = {
+        val x = currentCycle.get
+        if( x!=null ) Some(x) else None
+    }
 }
