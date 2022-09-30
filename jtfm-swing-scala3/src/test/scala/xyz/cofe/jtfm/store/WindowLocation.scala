1a9be3338c98e8bff9894edc343b9104f905f86c
Georgiy Kamnev <nt.gocha@gmail.com>
2022-09-30 15:09:37 +0500

    fix state

--- jtfm-swing-scala3/src/test/scala/xyz/cofe/jtfm/store/WindowLocation.scala
+++ jtfm-swing-scala3/src/test/scala/xyz/cofe/jtfm/store/WindowLocation.scala
@@ -8,7 +8,7 @@ enum WindowState:
   case Minimized
   case Maximized
 
-object WindowState
+object WindowState:
   given ToJson[WindowState] with
     def toJson(s:WindowState) = s match
       case WindowState.Maximized => Right(JS.Str("max"))
@@ -17,9 +17,17 @@ object WindowState
   given FromJson[WindowState] with
     def fromJson(j:JS) = j match
       case JS.Str("max") => Right(WindowState.Maximized)
-      case JS.Str("min") => Right(WindowState.Maximized)
-      case JS.Str("norm") => Right(WindowState.Maximized)
+      case JS.Str("min") => Right(WindowState.Minimized)
+      case JS.Str("norm") => Right(WindowState.Normal)
       case _ => Left(s"undefined state for $j")
+
+extension[T] (optValue:Option[T])
+  def lift:Either[String,T] = optValue match
+    case Some(v) => Right(v)
+    case None => Left("value not exists")
+  def lift(errMessage:String):Either[String,T] = optValue match
+    case Some(v) => Right(v)
+    case None => Left(errMessage)
 
 case class WindowLocation(state:WindowState, left:Int, top:Int, width:Int, height:Int)
 object WindowLocation:
@@ -39,6 +47,27 @@ object WindowLocation:
           "width" -> j_w,
           "height" -> j_h
         ))
+  given FromJson[WindowLocation] with
+    def fromJson(j:JS) = j match
+      case JS.Obj(fields) => 
+        for
+          st_j <- fields.get("state").lift("state not readed")
+          st_v <- summon[FromJson[WindowState]].fromJson(st_j)
+
+          l_j <- fields.get("left").lift("left not readed")
+          l_v <- summon[FromJson[Int]].fromJson(l_j)
+
+          t_j <- fields.get("top").lift("top not readed")
+          t_v <- summon[FromJson[Int]].fromJson(t_j)
+
+          w_j <- fields.get("width").lift("width not readed")
+          w_v <- summon[FromJson[Int]].fromJson(w_j)
+
+          h_j <- fields.get("height").lift("height not readed")
+          h_v <- summon[FromJson[Int]].fromJson(h_j)
+        yield
+          WindowLocation(st_v, l_v, t_v, w_v, h_v)
+      case _ => Left(s"expect obj in $j")
 
 class WindowLocTest extends munit.FunSuite {
   test("json encode") {
@@ -70,5 +99,10 @@ class WindowLocTest extends munit.FunSuite {
     val loc = WindowLocation(WindowState.Normal, 1, 2, 3, 4)
     val js = summon[ToJson[WindowLocation]].toJson(loc)
     println(js)
+
+    val loc_r = summon[FromJson[WindowLocation]].fromJson(js.getOrElse(null)).getOrElse(null)
+    println(loc_r)
+    println(loc)
+    println(loc_r == loc)
   }
 }
