fb94ff5d8e721c9ce386945dcb18c194fc8f3a63
Georgiy Kamnev <nt.gocha@gmail.com>
2022-10-05 02:36:16 +0500

    selection focus

--- jtfm-swing-scala3/src/main/scala/xyz/cofe/jtfm/ui/table/SwingTableFocus.scala
+++ jtfm-swing-scala3/src/main/scala/xyz/cofe/jtfm/ui/table/SwingTableFocus.scala
@@ -15,18 +15,20 @@ trait SwingTableFocus[A](using model:ListTableModel[A]) extends JTable:
       case None => this.getSelectionModel().setLeadSelectionIndex(-1)
       case Some(row0) => model.indexOf(row0).getOrElse(-1) match
         case ridx:Int if ridx<0 => this.getSelectionModel().setLeadSelectionIndex(-1)
-        case ridx:Int => this.getSelectionModel().setLeadSelectionIndex(ridx)
+        case ridx:Int => 
+          this.getSelectionModel().setLeadSelectionIndex(ridx)
     prev
 
   def focusedRow:Option[Int] = 
     val row = this.getSelectionModel().getLeadSelectionIndex()
     if row<0 then None else Some(row)
-    
+
   def focusedRow_=(row:Option[Int]):Option[Int] =
     val last = focusedRow
     row match
       case None => this.getSelectionModel().setLeadSelectionIndex(-1)
       case Some(row0) => row0 match
         case ridx:Int if ridx<0 => this.getSelectionModel().setLeadSelectionIndex(-1)
-        case ridx:Int => this.getSelectionModel().setLeadSelectionIndex(ridx)
+        case ridx:Int => 
+          this.getSelectionModel().setLeadSelectionIndex(ridx)
     last
