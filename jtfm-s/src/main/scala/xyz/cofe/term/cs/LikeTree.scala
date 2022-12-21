b3e87c2297545fcbdc95f2d95ee8c6a7bd565449
Georgiy Kamnev <nt.gocha@gmail.com>
2022-12-22 02:56:38 +0500

    forward deep tree step

--- jtfm-s/src/main/scala/xyz/cofe/term/cs/LikeTree.scala
+++ jtfm-s/src/main/scala/xyz/cofe/term/cs/LikeTree.scala
@@ -12,12 +12,89 @@ extension [T:LikeTree](node:T)
 
   def nodesCount():Int = summon[LikeTree[T]].nodes(node).size
   def nodes:List[T] = summon[LikeTree[T]].nodes(node)
-  //def nodes
+  def walk = LikeTree.Walk(node)
 
-class TreeNodeIter[T:LikeTree]( roots:List[T] ) extends Iterator[ List[T] ]:
-  var workSet:List[List[T]] = roots.map { root => List(root) }
-  def hasNext: Boolean = workSet.nonEmpty
-  def next(): List[T] = 
-    val res = workSet.head
-    workSet = res.nodes ++ workSet.tail
-    res
+trait TreePath[A:LikeTree]:
+  def node:A
+  def newChild(a:A):TreePath[A]
+  def parent:Option[TreePath[A]]
+  def listToLeaf:List[A]
+  def selfSibIndex:Option[Int] =
+    parent.flatMap { prnt =>
+      val i = prnt.node.nodes.indexOf(node) 
+      if i>=0 then Some(i) else None
+    }
+  def leftSib:Option[TreePath[A]] =
+    selfSibIndex.flatMap { selfIdx => 
+      if selfIdx<=0 
+      then None
+      else 
+        parent.flatMap { prnt =>
+          val sibs = prnt.node.nodes
+          if selfIdx >= sibs.size 
+          then None
+          else 
+            val sib = sibs(selfIdx-1)
+            Some(prnt.newChild(sib))
+        }
+    }
+  def rightSib:Option[TreePath[A]] =
+    selfSibIndex.flatMap { selfIdx => 
+      if selfIdx<0 
+      then None
+      else 
+        parent.flatMap { prnt =>
+          val sibs = prnt.node.nodes
+          if selfIdx >= (sibs.size-1)
+          then None
+          else 
+            val sib = sibs(selfIdx+1)
+            Some(prnt.newChild(sib))
+        }
+    }
+
+  def firstLeftChild:Option[TreePath[A]] =
+    val childs = node.nodes
+    if childs.isEmpty
+    then None
+    else Some(newChild(childs.head))
+
+  def righter:Option[TreePath[A]] =
+    rightSib.orElse(
+      parent.flatMap( prnt => prnt.righter )
+    )
+
+  def rightDeepest:Option[TreePath[A]] =
+    val childs = node.nodes
+    if childs.isEmpty
+    then None
+    else 
+      val right = newChild( childs.last )
+      Some( right.rightDeepest.getOrElse( right ) )
+
+  def nextByDeep:Option[TreePath[A]] =
+    firstLeftChild.orElse( righter )
+
+
+case class RTreePath[A:LikeTree]( rpath:List[A] ) extends TreePath[A]:
+  def node:A = rpath.last
+  def newChild(a:A):RTreePath[A] = RTreePath( rpath :+ a )
+  def parent:Option[RTreePath[A]] =
+    if rpath.size>1 then
+      Some(RTreePath(rpath.take(rpath.size-1)))
+    else
+      None
+  def listToLeaf:List[A] = rpath
+
+object LikeTree:
+  class Walk[T:LikeTree](node:T):
+    def path = TreeNodeIter(List(node))
+
+  class TreeNodeIter[T:LikeTree]( roots:List[T] ) extends Iterator[ RTreePath[T] ]:
+    var workSet:List[RTreePath[T]] = roots.map { root => RTreePath(List(root)) }
+    def hasNext: Boolean = workSet.nonEmpty
+    def next(): RTreePath[T] = 
+      val res = workSet.head
+      val next = res.node.nodes.map { n => res.newChild(n) }
+      workSet = next ++ workSet.tail
+      res
--- jtfm-s/src/test/scala/xyz/cofe/term/cs/LikeTreeTest.scala
+++ jtfm-s/src/test/scala/xyz/cofe/term/cs/LikeTreeTest.scala
@@ -1,3 +1,162 @@
 package xyz.cofe.term.cs
 
+class LikeTreeTest extends munit.FunSuite:
+  enum Node(val value:String):
+    case Leaf(override val value:String) extends Node(value)
+    case Branch(override val value:String, left:Node, right:Node) extends Node(value)
 
+  object Node:
+    given LikeTree[Node] with
+      def nodes(n:Node):List[Node] =
+        n match
+          case Branch(value, left, right) => List(left,right)
+          case Leaf(value) => List()
+
+  import Node._
+
+  /*
+                          root (0)
+              l(1)                         r(8)
+       ll(2)         lr(5)         rl(9)          rr(12)
+  lll(3) llr(4) lrl(6) lrr(7) rll(10) rlr(11) rrl(13) rrr(14)
+
+  /////////////////////////
+  deep order
+
+  0  root
+  1  root/l
+  2  root/l/ll
+  3  root/l/ll/lll
+  4  root/l/ll/llr
+  5  root/l/lr
+  6  root/l/lr/lrl
+  7  root/l/lr/lrr
+  8  root/r
+  9  root/r/rl
+  10 root/r/rl/rll
+  11 root/r/rl/rlr
+  12 root/r/rr
+  13 root/r/rr/rrl
+  14 root/r/rr/rrr
+
+
+
+  forward
+
+  case root -> l  | root.fistLeft
+  case l -> ll    | l.firstLeft
+  case ll -> lll  | ll.firstLeft
+  case lll -> llr | llr.rightSib
+  case llr -> lr  | lr.parent.rightSib
+  case lr -> lrl  | lr.firstLeft
+  case lrl -> lrr | lrl.rightSib
+  case lrr -> r   | lrr.parent.parent(l).rightSib - exists r
+  case r -> rl    | r.firstLeft
+  case rl -> rll  | rl.firstLeft
+  case rll -> rlr | rl.rightSib
+  case rlr -> rr  | rlr.parent.rightSib
+  case rr -> rrl  | rr.firstLeft
+  case rrl -> rrr | rrl.rightSib 
+
+
+  backward
+
+  case rrr -> rrl | rr.leftSib
+  case rrl -> rr  | rrl.parent , rr.leftSib not exists
+  case rr -> rlr  | rr.leftSib.deeperRight
+  case rlr -> rll | rr.leftSib
+  case rll -> rl  | rll.parent
+  case rl -> r    | rl.parent , rl.leftSib not exists
+  case r ->  lrr  | r.leftSib.deeperRight 
+  case lrr -> lrl | lrl.leftSib
+  case lrl -> lr  | lrl.parent
+  case lr -> llr  | lr.leftSib.deeperRight
+  case llr -> lll | llr.leftSib
+  case lll -> ll  | lll.parent
+  case ll -> l    | ll.parent
+  case l -> root  | l.parent
+
+  */
+  val tree = Branch(
+    "root", 
+    Branch("l", 
+      Branch("ll", Leaf("lll"), Leaf("llr")),
+      Branch("lr", Leaf("lrl"), Leaf("lrr"))
+    ),
+    Branch("r",
+      Branch("rl", Leaf("rll"), Leaf("rlr")),
+      Branch("rr", Leaf("rrl"), Leaf("rrr"))
+    )
+  )
+
+  test("likeTree") {
+    assert( tree.nodesCount()==2 )
+    assert( tree.node(0).map(n => n.value) == Some("l") )
+    assert( tree.node(1).map(n => n.value) == Some("r") )
+    assert( tree.node(2).map(n => n.value) == None )
+  }
+
+  test("walk") {
+    tree.walk.path.foreach(path => {
+      println( path.listToLeaf.map(_.value).mkString("/") )
+    })
+  }
+
+  test("leftSib") {
+    val lrPath = tree.walk.path.find { _.node.value == "lr" }.get
+    val llPath = tree.walk.path.find { _.node.value == "ll" }.get
+    assert( lrPath.leftSib == Some(llPath) )
+  }
+
+  test("rightSib") {
+    val lrPath = tree.walk.path.find { _.node.value == "lr" }.get
+    val llPath = tree.walk.path.find { _.node.value == "ll" }.get
+    assert( llPath.rightSib == Some(lrPath) )
+  }
+
+  test("firstLeftChild") {
+    val lPath = tree.walk.path.find { _.node.value == "l" }.get
+    val llPath = tree.walk.path.find { _.node.value == "ll" }.get
+    assert( lPath.firstLeftChild == Some(llPath) )
+  }
+
+  test("righter") {
+    val lrlPath = tree.walk.path.find { _.node.value == "lrl" }.get
+    val lrrPath = tree.walk.path.find { _.node.value == "lrr" }.get
+    val rPath = tree.walk.path.find { _.node.value == "r" }.get
+
+    assert(lrlPath.righter == Some(lrrPath))
+    assert(lrrPath.righter == Some(rPath))
+  }
+
+  test("nextByDeep") {
+    val l   = tree.walk.path.find { _.node.value == "l" }.get
+    val ll  = tree.walk.path.find { _.node.value == "ll" }.get
+    val lll = tree.walk.path.find { _.node.value == "lll" }.get
+    val llr = tree.walk.path.find { _.node.value == "llr" }.get
+    val lr  = tree.walk.path.find { _.node.value == "lr" }.get
+    val lrl = tree.walk.path.find { _.node.value == "lrl" }.get
+    val lrr = tree.walk.path.find { _.node.value == "lrr" }.get
+    val r   = tree.walk.path.find { _.node.value == "r" }.get
+    val rl  = tree.walk.path.find { _.node.value == "rl" }.get
+    val rll = tree.walk.path.find { _.node.value == "rll" }.get
+    val rlr = tree.walk.path.find { _.node.value == "rlr" }.get
+    val rr  = tree.walk.path.find { _.node.value == "rr" }.get
+    val rrl = tree.walk.path.find { _.node.value == "rrl" }.get
+    val rrr = tree.walk.path.find { _.node.value == "rrr" }.get
+
+    assert( l.nextByDeep   == Some(ll),  "l -> ll" )
+    assert( ll.nextByDeep  == Some(lll), "ll -> ll" )
+    assert( lll.nextByDeep == Some(llr), "lll -> llr" )
+    assert( llr.nextByDeep == Some(lr),  "llr -> lr" )
+    assert( lr.nextByDeep  == Some(lrl), "lr -> lrl" )
+    assert( lrl.nextByDeep == Some(lrr), "lrl -> lrr" )
+    assert( lrr.nextByDeep == Some(r),   "lrr -> r" )
+    assert( r.nextByDeep   == Some(rl),  "r -> rl" )
+    assert( rl.nextByDeep  == Some(rll), "rl -> rll" )
+    assert( rll.nextByDeep == Some(rlr))
+    assert( rlr.nextByDeep == Some(rr))
+    assert( rr.nextByDeep  == Some(rrl))
+    assert( rrl.nextByDeep == Some(rrr))
+    assert( rrr.nextByDeep == None)
+  }
