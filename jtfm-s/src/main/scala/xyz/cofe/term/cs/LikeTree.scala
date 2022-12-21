f9f55e5fe73e14ab318944241ea20c22c3d0d7ee
Georgiy Kamnev <nt.gocha@gmail.com>
2022-12-21 16:28:12 +0500

    fix

--- jtfm-s/src/main/scala/xyz/cofe/term/cs/LikeTree.scala
+++ jtfm-s/src/main/scala/xyz/cofe/term/cs/LikeTree.scala
@@ -1,5 +1,23 @@
 package xyz.cofe.term.cs
 
 trait LikeTree[N]:
-  def node(index:Int):Option[N]
-  def nodesCount:Int
+  def nodes(n:N):List[N]
+
+extension [T:LikeTree](node:T)
+  def node(index:Int):Option[T] = 
+    val l = summon[LikeTree[T]].nodes(node)
+    if index>=0 && index<l.size 
+    then Some(l(index))
+    else None
+
+  def nodesCount():Int = summon[LikeTree[T]].nodes(node).size
+  def nodes:List[T] = summon[LikeTree[T]].nodes(node)
+  //def nodes
+
+class TreeNodeIter[T:LikeTree]( roots:List[T] ) extends Iterator[ List[T] ]:
+  var workSet:List[List[T]] = roots.map { root => List(root) }
+  def hasNext: Boolean = workSet.nonEmpty
+  def next(): List[T] = 
+    val res = workSet.head
+    workSet = res.nodes ++ workSet.tail
+    res
