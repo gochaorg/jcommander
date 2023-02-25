f228b1d9c1e2377ee383d4562c60a194b5e39518
Georgiy Kamnev <nt.gocha@gmail.com>
2023-02-26 03:27:15 +0500

    remove jna

--- jtfm-s/src/main/scala/xyz/cofe/files/FileType.scala
+++ jtfm-s/src/main/scala/xyz/cofe/files/FileType.scala
@@ -1,6 +1,29 @@
 package xyz.cofe.files
 
+import java.nio.file.Path
+
+/**
+  * Определяет тип файла/каталога
+  */
 enum FileType:
   case RegularFile
   case Directory
   case SymbolicLink
+
+object FileType:  
+  object UndefinedType extends Error
+
+  def from(path: Path):Either[Throwable,FileType] =
+    from_basicImpl(path)
+
+  private def from_basicImpl(path:Path):Either[Throwable,FileType] =
+    path.isRegularFile.flatMap {
+      case true => Right(FileType.RegularFile)
+      case false => path.isSymbolicLink.flatMap {
+        case true => Right(FileType.SymbolicLink)
+        case false => path.isDirectory.flatMap {
+          case true => Right(FileType.Directory)
+          case false => Left(UndefinedType)
+        }
+      }
+    }
